1fd6d715d8d3b534d063454f7e877af6921d5297
Enrique Gimenez <francisco.gimenez@mangoapp.com.py>
2025-07-15 14:37:20 -0400

    initial setup

---
+++ handler.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import serverless from "serverless-http";
+
+const app = express();
+
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from root!",
+  });
+});
+
+app.get("/hello", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from path!",
+  });
+});
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+export const handler = serverless(app);
