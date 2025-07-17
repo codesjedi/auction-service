5137ea0663cceee05e8dc2abc5e0951e0f5820b4
Enrique Gimenez <francisco.gimenez@mangoapp.com.py>
2025-07-17 13:05:08 -0300

    Add getEndedAuctions function, update IAM permissions, and configure serverless package patterns

--- src/handlers/auctions/process.ts
+++ src/handlers/auctions/process.ts
@@ -1,7 +1,11 @@
 import { APIGatewayProxyEvent } from 'aws-lambda'
 
+import { getEndedAuctions } from '../../lib/getEndedAuctions'
+
 async function processAuctions(event: APIGatewayProxyEvent) {
   console.log('processing auctions!')
+  const auctionsToClose = await getEndedAuctions()
+  console.log(auctionsToClose)
 }
 
 export const handler = processAuctions
